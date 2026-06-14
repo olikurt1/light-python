2093ac11180cd78bed88ad5024cc9bff47054f6d
olikurt1 <145839522+olikurt1@users.noreply.github.com>
2026-06-14 11:24:48 +0100

    FizzBuzz done

---
+++ FizzBuzz.py
@@ -0,0 +1,12 @@
+def fizzbuzz():
+    for x in range(1, 21):
+        if x % 5 == 0 and x % 3 == 0:
+            print("FizzBuzz")
+        elif x % 3 == 0:
+            print("Fizz")
+        elif x % 5 == 0:
+            print("Buzz")
+        else:
+            print(x)
+
+fizzbuzz()
