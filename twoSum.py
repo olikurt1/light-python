74b317f4de2e2e5c75f78d6524922487efbf840a
olikurt1 <145839522+olikurt1@users.noreply.github.com>
2026-06-14 12:33:30 +0100

    Making twosum faster

--- twoSum.py
+++ twoSum.py
@@ -1,8 +1,14 @@
 def twoSum(nums, target):
+       #dicitionary to hold a value thats already been seen 
+        seen = {}
         #first number looking for its pair
-        for x in range(len(nums)):
-            #runs through the list of numbers again except for the number that is looking for its pair
-            for y in range(x+ 1, len(nums)):
-                if nums[x] + nums[y] == target:
-                    return (nums[x], nums[y])
- 
+        for num in nums:
+            #complement is automatically the right number for the current iteration num to make correct pair 
+            complement = target - num
+            #so if complement is in seen then can immediately return
+            if complement in seen:
+                return (complement, num)
+            #always add current number to seen as it has been seen even if incorrect and can be used for future referece
+            seen[num] = True
+        return None
+          
